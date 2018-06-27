157d0db7b3180abb8327a26b975aa73c8683e286
Marissa Zhong <mwz2101@columbia.edu>
2018-06-27 20:31:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,14 @@
 
 class Song
 
-  @@count
+  @@count = 0
+  
   attr_accessor :name, :artist, :genre
   def initialize(name,artist,genre)
     @name = name
     @artist = artist
     @genre = genre
+    @@count += 1
   end
   
 end
